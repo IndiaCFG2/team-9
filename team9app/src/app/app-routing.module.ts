8769ddeb6ea9cd48df6eae08d70eb32b3b84e8f0
SANKAR ROHIT JALLA <jssankarrohit25@gmail.com>
2020-08-08 18:38:56 +0530

    navbar routings and components

--- team9app/src/app/app-routing.module.ts
+++ team9app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'student', component:StudentComponent},
+  {path:'teacher', component:TeacherComponent},
+  {path:'about', component:AboutComponent},
+  {path:'admin', component:AdminComponent},
+  {path:'aboutus', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- team9app/src/app/app.module.ts
+++ team9app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { AdminComponent } from './admin/admin.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TeacherComponent,
+    AdminComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
